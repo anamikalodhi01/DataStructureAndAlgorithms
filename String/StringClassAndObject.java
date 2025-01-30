15cb1dfe3c0f39341dc8efa9773bfaaba8fd1dec
Anamika <anamikalodhi77@gmail.com>
2025-01-30 11:45:40 +0530

    s

---
+++ String/StringClassAndObject.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringClassAndObject {
+    public static void main(String[] args) {
+        //class
+        System.out.println(String.class);
+
+        //object
+        String s = "hello java"; //heap
+        System.out.println(s instanceof Object);
+
+
+    }
+}
